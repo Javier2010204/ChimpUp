a470e4f184ee08c856ec36aa5d7b11d0a14af3a0
Javier2010204 <2010204javiergomez@gmail.com>
2019-03-19 02:27:12 -0600

    pagina principal

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,14 @@
 class Product < ApplicationRecord
+
+    validates :name, presence: true
+    validates :name, length: {minimum: 10, maximum: 120}
+    validates :description, presence: true
+    validates :description, length: {minimum: 50}
+    validates :sale_price, presence: true
+    validates :cost_price, presence: true
+    validates :brand_id, presence: true
+    validates :sub_category_id, presence: true
+
+    has_attached_file :avatar, styles: {thumb: "100x100", medium: "300x300"}, default_url: "/images/:style/default-avatar.png"
+    validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 end

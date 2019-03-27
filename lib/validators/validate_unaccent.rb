6abe59b4fad2d5361c8464dbc57e0cd2963313d6
Javier2010204 <2010204javiergomez@gmail.com>
2019-03-27 12:32:07 -0600

    configuracion de unnacent

---
+++ lib/validators/validate_unaccent.rb
@@ -0,0 +1,9 @@
+class ValidateUnaccent < ActiveModel::Validator
+
+    def validate(record)
+      records = options[:model].all
+      records.each do |r|
+          if I18n.transliterate(r.name.downcase) == I18n.transliterate(record.name.downcase) && r.id != record.errors[:name] << 'Ya Existe'
+      end
+    end
+end
